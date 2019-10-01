9d37705e226ace2720eef48b32dac52c0063741b
OlgaSemenko <Oli@192.168.0.4>
2019-10-01 10:30:32 +0200

    YearHint - written logic for skipping more than 4 consecutive numbers

--- log-combiner-parent/log-labelizer/src/main/java/com/credibledoc/log/labelizer/hint/YearHint.java
+++ log-combiner-parent/log-labelizer/src/main/java/com/credibledoc/log/labelizer/hint/YearHint.java
@@ -10,36 +10,66 @@ public class YearHint {
 	public static final int SHORT_OLD_YEAR = 80;
 	DateFormat shortActualYear = new SimpleDateFormat("yy");
 	public static final int SHORT_ACTUAL_YEAR = Calendar.getInstance().get(Calendar.YEAR) % 100;
-	public static final int SHORT_ZERO_YEAR = 00; //year 2000 in short version
-	public static final int SHORT_HELPFULL_YEAR = 100; //year 2000 in helpfull version for nineteens years
+	public static final int SHORT_ZERO_YEAR = 00; // year 2000 in short version
+	public static final int SHORT_HELPFULL_YEAR = 100; // year 2000 in helpfull version for nineteens years
 
 	public static void main(String[] args) {
-		String input =  "81adjhiiuhef1990fd70sdfafgrev1911";
-		String output = "ddwwwwwwwwwwddddwwwwwwwwwwwwwwwww";
+		String input = "81adjhiiuhef19901980fd70sdfafgrev1990";
+		String output = "ddwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwdddd";
+		int lastInputValue = (input.length() - 1);
 		StringBuffer result = new StringBuffer();
 		StringBuffer context = new StringBuffer();
-		begin:
-		for (int i = 0; i < input.length(); i++) {
+		begin: for (int i = 0; i < input.length(); i++) {
 			Character character = input.charAt(i);
 			System.out.println(character + ":" + Character.isDigit(character));
 			if (!Character.isDigit(character)) {
 				result.append("w");
-				context.delete(0, context.length()); // zajišťuje, že context nebude počítat s hodnotami, které byli v předešlém zkoumaném čísle
+				context.delete(0, context.length()); // zajišťuje, že context nebude počítat s hodnotami, které byli v
+														// předešlém zkoumaném čísle
 			} else {
 				context.append(character);
 				if (context.length() == 2 || context.length() == 4) {
 					if (context.length() == 2) {
+						i++;
+						character = input.charAt(i);
+						System.out.println(character + ":" + Character.isDigit(character));
+						if (!Character.isDigit(character)) {
+							if (dateControl(context, result) == false) {
+							}
+							result.append("w");
+						}
+						context.append(character);
+						continue begin;
+					}
+					if (context.length() == 4) {
+						if (i != lastInputValue) {
 							i++;
 							character = input.charAt(i);
 							System.out.println(character + ":" + Character.isDigit(character));
-							if(!Character.isDigit(character)) {
+							if (!Character.isDigit(character)) {
 								if (dateControl(context, result) == false) {
+									if (!Character.isDigit(character)) {
+										dateControl(context, result);
+									}
 								}
 								result.append("w");
+							} else {
+								while (Character.isDigit(character)) {
+									context.append(character);
+									i++;
+									character = input.charAt(i);
+									System.out.println(character + ":" + Character.isDigit(character));
+								}
 							}
-							context.append(character);
-							continue begin;
+						} else {
+							dateControl(context, result);
+							System.out.println(output); // Pro ověření že výstup sedí
+							System.out.println(result.toString());
+							System.out.println(result.toString().equals(output));
+							System.exit(0);
+						}
 					}
+					result.append("w");
 					dateControl(context, result);
 				} else {
 					continue;
@@ -52,23 +82,24 @@ public class YearHint {
 		System.out.println(result.toString().equals(output));
 	}
 
+
 	private static boolean dateControl(StringBuffer context, StringBuffer result) {
 		Integer contextResult = Integer.valueOf(context.toString());
 		boolean isDate = isDate(contextResult);
 		if (isDate == true) {
 			for (int d = 0; d < context.length(); d++) {
-				result.append("d"); //píše "d" pouze pro případ, který odpovídá metodě isDate
+				result.append("d"); // píše "d" pouze pro případ, který odpovídá metodě isDate
 			}
 			return true;
 		}
 		for (int w = 0; w < context.length(); w++) {
-			result.append("w"); //píše "d" pouze pro případ, který odpovídá metodě isDate
+			result.append("w"); // píše "d" pouze pro případ, který odpovídá metodě isDate
 		}
 		return false;
 	}
 
 	private static boolean isDate(Integer contextResult) {
-		if ((contextResult >= OLDEST_YEAR && contextResult <= ACTUAL_YEAR) 
+		if ((contextResult >= OLDEST_YEAR && contextResult <= ACTUAL_YEAR)
 				|| (contextResult >= SHORT_ZERO_YEAR && contextResult <= SHORT_ACTUAL_YEAR)
 				|| (contextResult >= SHORT_OLD_YEAR && contextResult < SHORT_HELPFULL_YEAR)) {
 			return true;
