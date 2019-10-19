786ca2efaed46a728bf8ec6b30e7327f193befea
OlgaSemenko <semenko.olga@seznam.cz>
2019-10-19 09:48:13 +0200

    log-labelizer - HintTest - sonar errors fixed

--- log-combiner-parent/log-labelizer/src/main/java/com/credibledoc/log/labelizer/hint/Hint.java
+++ log-combiner-parent/log-labelizer/src/main/java/com/credibledoc/log/labelizer/hint/Hint.java
@@ -2,9 +2,7 @@ package com.credibledoc.log.labelizer.hint;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import com.credibledoc.log.labelizer.date.ProbabilityLabel;
 
@@ -27,99 +25,107 @@ public class Hint {
 
 	public static String yearLabels(String input) {
 		Integer lastInputValue = (input.length() - 1);
-		StringBuffer result = new StringBuffer();
-		StringBuffer context = new StringBuffer();
-		for (int i = 0; i < input.length(); i++) {
-			Integer valueOfInput = i;
-			Character character = input.charAt(i);
-			System.out.println(character + ":" + Character.isDigit(character));
+		StringBuilder result = new StringBuilder();
+		StringBuilder context = new StringBuilder();
+		for (int index = 0; index < input.length(); index++) {
+			Integer valueOfInput = index;
+			Character character = input.charAt(index);
 			if (!Character.isDigit(character)) {
 				result.append(WITHOUT);
-				context.delete(0, context.length()); 
-				controlLastValueExit(result, lastInputValue, valueOfInput, context);
+				context.delete(0, context.length());
+				controlLastValue(result, lastInputValue, valueOfInput, context);
 			} else {
 				context.append(character);
-				
+
 				if (context.length() <= 4) {
 					nextCharControl(valueOfInput, lastInputValue, input, context, result);
 				} else {
-					while (i < lastInputValue && Character.isDigit(character)) {
-						controlLastValueExit(result, lastInputValue, valueOfInput, context);
-						i++;
-						valueOfInput = i;
-						controlLastValueExit(result, lastInputValue, valueOfInput - 1, context);
-						character = input.charAt(i);
-						context.append(character);
-						System.out.println(character + ":" + Character.isDigit(character));
-					}
-					
-					for (int w = 0; w < context.length(); w++) {
-						result.append(WITHOUT);
-					}
-					context.delete(0, context.length()); 
-					controlLastValueExit(result, lastInputValue, valueOfInput, context);
+					index = labelizingOfFifeAndMoreNumbers(input, lastInputValue, result, context, index, valueOfInput, // NOSONAR
+							character);
 				}
 
 			}
 		}
 		return result.toString();
 	}
-	
-// This is method, which controls characters of context. Also it controls next characters if there is number or not.
-	private static void nextCharControl(Integer valueOfInput,Integer lastInputValue, String input, StringBuffer context, StringBuffer result) {
+
+	private static int labelizingOfFifeAndMoreNumbers(String input, Integer lastInputValue, StringBuilder result,
+			StringBuilder context, int index, Integer valueOfInput, Character character) {
+		while (index < lastInputValue && Character.isDigit(character)) {
+			controlLastValue(result, lastInputValue, valueOfInput, context);
+			index++;
+			valueOfInput = index;
+			controlLastValue(result, lastInputValue, valueOfInput - 1, context);
+			character = input.charAt(index);
+			context.append(character);
+		}
+
+		for (int w = 0; w < context.length(); w++) {
+			result.append(WITHOUT);
+		}
+		context.delete(0, context.length());
+		controlLastValue(result, lastInputValue, valueOfInput, context);
+		return index;
+	}
+
+	/**
+	 * This is method, which controls characters of context. Also it controls next
+	 * characters if there is number or not.
+	 */
+	private static void nextCharControl(int valueOfInput, int lastInputValue, String input, StringBuilder context,
+			StringBuilder result) {
 		int c = valueOfInput;
 		if (valueOfInput == lastInputValue) {
 			controlOfSingleNumber(context, result);
-			context.delete(0, context.length()); 
-			controlLastValueExit(result, lastInputValue, valueOfInput, context);
+			context.delete(0, context.length());
+			controlLastValue(result, lastInputValue, valueOfInput, context);
 		} else {
-		c++;
+			c++;
 		}
 		Character nextChar = input.charAt(c);
 		if (!Character.isDigit(nextChar)) {
 			controlOfSingleNumber(context, result);
 		}
 	}
-	
-//  When in context is just one number, this method controls, if next character is number or not. 
-//	If number does not have number in the next step, it mean, that number certainly is not an year.
-	private static void controlOfSingleNumber(StringBuffer context, StringBuffer result) {
+
+	/**
+	 * When in context is just one number, this method controls, if next character
+	 * is number or not. If number does not have number in the next step, it mean,
+	 * that number certainly is not an year.
+	 */
+	private static void controlOfSingleNumber(StringBuilder context, StringBuilder result) {
 		if (context.length() != 1) {
 			dateControl(context, result);
 		} else {
 			result.append(WITHOUT);
 		}
 	}
-	private static void dateControl(StringBuffer context, StringBuffer result) {
+
+	private static void dateControl(StringBuilder context, StringBuilder result) {
 		Integer contextResult = Integer.valueOf(context.toString());
-		boolean isDate = isDate(contextResult);
-		if (isDate == true) {
+		if (isDate(contextResult)) {
 			for (int d = 0; d < context.length(); d++) {
 				result.append(YEAR);
 			}
 		} else {
 			for (int w = 0; w < context.length(); w++) {
-				result.append(WITHOUT); 
+				result.append(WITHOUT);
 			}
 		}
 	}
 
 	private static boolean isDate(Integer contextResult) {
-		if ((contextResult >= OLDEST_YEAR && contextResult <= ACTUAL_YEAR + 1)
+		return (contextResult >= OLDEST_YEAR && contextResult <= ACTUAL_YEAR + 1)
 				|| (contextResult >= SHORT_ZERO_YEAR && contextResult <= SHORT_ACTUAL_YEAR + 1)
-				|| (contextResult >= SHORT_OLD_YEAR && contextResult < SHORT_HELPFULL_YEAR)) {
-			return true;
-		}
-		return false;
+				|| (contextResult >= SHORT_OLD_YEAR && contextResult < SHORT_HELPFULL_YEAR);
 	}
 
-	private static void controlLastValueExit(StringBuffer result, Integer lastInputValue, Integer valueOfInput,StringBuffer context) {
+	private static void controlLastValue(StringBuilder result, int lastInputValue, int valueOfInput,
+			StringBuilder context) {
 		if (valueOfInput == lastInputValue) {
 			for (int w = 0; w < context.length(); w++) {
 				result.append(WITHOUT);
 			}
-			System.out.println("reslut:   " + result.toString());
-			List<ProbabilityLabel> l2 = new ArrayList<ProbabilityLabel>();
 		}
 	}
 
