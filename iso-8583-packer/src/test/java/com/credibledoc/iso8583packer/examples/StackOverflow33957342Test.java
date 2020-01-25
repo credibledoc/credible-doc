a341f584418bfd59a94243006e17d45d1b8c2418
Kyrylo Semenko <kyrylo.semenko@aevi.com>
2020-01-25 11:29:21 +0100

    iso-8583-packer - StackOverflow 33957342 example

---
+++ iso-8583-packer/src/test/java/com/credibledoc/iso8583packer/examples/StackOverflow33957342Test.java
@@ -0,0 +1,83 @@
+package com.credibledoc.iso8583packer.examples;
+
+import com.credibledoc.iso8583packer.FieldBuilder;
+import com.credibledoc.iso8583packer.ValueHolder;
+import com.credibledoc.iso8583packer.bcd.BcdBodyPacker;
+import com.credibledoc.iso8583packer.dump.DumpService;
+import com.credibledoc.iso8583packer.hex.HexService;
+import com.credibledoc.iso8583packer.message.MsgField;
+import com.credibledoc.iso8583packer.message.MsgFieldType;
+import com.credibledoc.iso8583packer.message.MsgValue;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * See the
+ * <a href="https://stackoverflow.com/questions/33957342/iso-8583-how-are-bcd-values-calculated-for-fields-with-subfields">
+ *     ISO 8583 - How are BCD Values Calculated For Fields With Subfields?
+ * </a>
+ * question on StackOverflow.
+ * 
+ * @author Kyrylo Semenko
+ */
+public class StackOverflow33957342Test {
+    private static final Logger logger = LoggerFactory.getLogger(StackOverflow33957342Test.class);
+
+    /**
+     * Creation of a message structure definition.
+     */
+    private FieldBuilder defineMessageStructure() {
+        return FieldBuilder.builder(MsgFieldType.VAL)
+            .defineName("Parent")
+            .defineLen(30)
+            
+            .createChild(MsgFieldType.VAL)
+            .defineName("Child1")
+            .defineLen(9)
+            .defineBodyPacker(BcdBodyPacker.noPadding())
+            
+            .cloneToSibling()
+            .defineName("Child2")
+            .defineLen(10)
+            .defineBodyPacker(BcdBodyPacker.noPadding())
+            
+            .cloneToSibling()
+            .defineName("Child3")
+            .defineLen(11)
+            .defineBodyPacker(BcdBodyPacker.noPadding())
+
+            .jumpToRoot()
+            .validateStructure();
+    }
+    
+    @Test
+    public void packUnpackTest() {
+        FieldBuilder fieldBuilder = defineMessageStructure();
+        ValueHolder valueHolder = ValueHolder.newInstance(fieldBuilder.getCurrentField());
+
+        valueHolder.jumpToChild("Child1").setValue("value1_11111111111");
+        valueHolder.jumpToSibling("Child2").setValue("value2_2222222222222");
+        valueHolder.jumpToSibling("Child3").setValue("value3_333333333333333");
+        
+        // print result
+        MsgValue msgValue = valueHolder.jumpToRoot().getCurrentMsgValue();
+        String msgFieldStructure = DumpService.getInstance().dumpMsgField(fieldBuilder.getCurrentField());
+        String msgValueData = DumpService.getInstance().dumpMsgValue(fieldBuilder.getCurrentField(), msgValue, false);
+        byte[] bytes = valueHolder.pack();
+        logger.info("Example of a message.\nMessage Structure:\n{}\nMessage data:\n{}\nMessage bytes in hex:\n{}",
+            msgFieldStructure, msgValueData, HexService.bytesToHex(bytes));
+        
+        // unpack again
+        MsgField msgField = defineMessageStructure().getCurrentField();
+        ValueHolder unpacker = ValueHolder.newInstance(msgValue, msgField).adjust();
+        unpacker.unpack(bytes);
+        
+        assertEquals("value1_11111111111", unpacker.jumpToChild("Child1").getValue(String.class));
+        assertEquals("value2_2222222222222", unpacker.jumpToSibling("Child2").getValue(String.class));
+        assertEquals("value3_333333333333333", unpacker.jumpToSibling("Child3").getValue(String.class));
+    }
+
+}
